cbf31166d95cd1fc4c91eba71322a3d10db53cd0
Kuldeep Chotaliya <kuldeepchotaliya17@gmail.com>
2024-03-29 16:50:01 +0530

    find second largest and check array is sorted

--- array_programs.rb
+++ array_programs.rb
@@ -10,5 +10,36 @@ end
 
 # You can also do this solution using first sort the array return arr[-1]
 
-
 p "find larget number in array--->#{find_largest([2,36,4,6,4,32,63,9,23])}"
+
+# second largest number in array without sorting
+
+def second_largest(arr)
+  largest = arr[0]
+  second_largest = arr[0]
+  arr.each do |ele|
+    if ele > largest
+      second_largest = largest
+      largest = ele
+    end
+  end
+  second_largest
+end
+
+p "find second larget number in array--->#{second_largest([2,36,4,6,4,32,63,9,23])}"
+
+# Check array is sorted or not
+
+def is_sorted?(arr)
+  (arr.size - 1).times  do |i|
+    if arr[i] <= arr[i + 1]
+    else
+      return false
+    end
+  end
+  return true
+end
+
+p "check array is sorted or not---->#{is_sorted?([12,12,13,13,14])}"
+
+# Remove duplicates from the sorted array
