fb510eb15377a999b5145dd2c99efbb02675cdc9
Kuldeep Chotaliya <kuldeepchotaliya17@gmail.com>
2024-04-03 18:52:34 +0530

    changes in largest subarray sum

--- array/largest_subarray_sum.rb
+++ array/largest_subarray_sum.rb
@@ -1,13 +1,39 @@
 # Brute Force:- Find all sub array and return sum of subarray who have highest sum.
 
+# optimal approach:- kanade's algorithm
+
+# explanation of algo:-
+
+# algo says that we will iterate over the array and we will maintain two variable max_sum and current_sum. 
+# current_sum will be the sum when we are iterating over the array and add it to current_sum
+# We will only preserve our current_sum when we got it as positive any other case it will be 0. So when we get our current_sum negative we will make current_sum 0. (Because saving negative value does not make any sense. If we add something to negative it will make more negative)
+# When we get current_sum > max_sum then we will update max_sum = current_sum
+
 def max_subarray_sum(arr)
-  max_current = max_global = arr[0]
+  max_sum = 0
+  current_sum = 0
+  sub_start = nil
+  sub_end = nil
+
   for i in (0..arr.size - 1)
-    max_current = [arr[i], max_current + arr[i]].max
-    # Update the global maximum if the maximum sum ending at the current element is greater
-    max_global = [max_global, max_current].max
+    sub_start = i if current_sum == 0 # saving starting index where we get our current_sum positive
+
+    current_sum += arr[i]
+    p "current_sum---->#{current_sum}"
+    p "maximum_sum---->#{max_sum}"
+    
+    if current_sum > max_sum
+      max_sum = current_sum 
+      sub_end = i # saving end index when our current_sum is greater than max_sum
+    end
+
+    if (current_sum < 0)
+      current_sum = 0
+    end
   end
-  max_global
+  p "sub_start--->#{sub_start}"
+  p "sub_end---->#{sub_end}"
+  max_sum
 end
 
 p "max subarray sum----->#{max_subarray_sum([-2,-3,4,-1,-2,1,5,-3])}"
